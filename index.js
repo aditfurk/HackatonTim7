d44dfd7baf316c4d4a212303fb82077592ad0525
Aditya FH <aditfurkon2000@gmail.com>
2022-01-27 19:41:18 +0700

    input, search beres

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
-function input() {
+const input = document.getElementById("input-btn")
+let data = []
+
+function getData(data) {
+  let output = `<tr class="p-5 bg-blue-400">
+  <th class="p-5">No</th>
+  <th class="p-5">Nama</th>
+  <th class="p-5">Alamat</th>
+  <th class="p-5">Umur</th>
+  <th class="p-5">Jenis Kelamin</th>
+  <th class="p-5">No. Handphone</th>
+</tr>\n`
+  let count = 0
+  for (const person of data) {
+    count++
+    output += `<tr class="bg-red-500">
+    <td class="p-5">${count}</td>
+    <td class="p-5">${person.nama}</td>
+    <td class="p-5">${person.alamat}</td>
+    <td class="p-5">${person.umur}</td>
+    <td class="p-5">${person.jenisKelamin}</td>
+    <td class="p-5">${person.noHp}</td>
+  </tr>`
+  }
+  return output
+}
+
+input.addEventListener('click', function(){
   const element = document.getElementById("update");
   const element2 = document.getElementById("input")
   element.classList.add("hidden");
   element2.classList.remove("hidden")
-}
+  let tableList = document.getElementById("list-table")
+  
+  let output = getData(data)
+  tableList.innerHTML = output
+})
+
 function update() {
   const element = document.getElementById("update");
   const element2 = document.getElementById("input")
@@ -13,8 +45,6 @@ function update() {
 
 
 const submit = document.getElementById("btn-submit")
-
-let data = []
 
 submit.addEventListener('click', function(){
   let tableHeading = document.getElementById("heading-table")
@@ -51,25 +81,61 @@ submit.addEventListener('click', function(){
 
   let tableList = document.getElementById("list-table")
   
-  let output = `<tr class="p-5 bg-blue-400">
-  <th class="p-5">No</th>
-  <th class="p-5">Nama</th>
-  <th class="p-5">Alamat</th>
-  <th class="p-5">Umur</th>
-  <th class="p-5">Jenis Kelamin</th>
-  <th class="p-5">No. Handphone</th>
-</tr>\n`
-  let count = 0
-  for (const person of data) {
-    count++
-    output += `<tr class="bg-red-500">
-    <td class="p-5">${count}</td>
-    <td class="p-5">${person.nama}</td>
-    <td class="p-5">${person.alamat}</td>
-    <td class="p-5">${person.umur}</td>
-    <td class="p-5">${person.jenisKelamin}</td>
-    <td class="p-5">${person.noHp}</td>
-  </tr>`
-  }
+  let output = getData(data)
+
   tableList.innerHTML = output
 })
+
+const search = document.getElementById("search-btn")
+
+search.addEventListener('click', function (){
+  const name = document.getElementById("nama-update")
+  const foundData = cariData (name.value, data)
+  let tableList = document.getElementById("list-table")
+  
+  let output = getData(foundData)
+  
+  tableList.innerHTML = output
+})
+
+function cariData(nama, data) {
+  let result = []
+  for (const person of data) {
+    let namaData = person.nama.toLowerCase()
+    let namaInput = nama.toLowerCase()
+    // let noKTP = person.noKTP
+    let found = false
+    for (let i = 0; i < namaData.length; i++) {
+      if (namaData[i] === namaInput[0]) {
+        let tempStr = ''
+        for (let j = i; j < i + namaInput.length; j++) {
+          tempStr += namaData[j]
+        }
+        if (tempStr === namaInput) {
+          result.push(person)
+          found = true
+          break;
+        }
+      }
+    }
+    // if (!found) {
+    //   for (let i = 0; i < noKTP.length; i++) {
+    //     if (noKTP[i] === namaInput[0]) {
+    //       let tempStr = ''
+    //       for (let j = i; j < i + namaInput.length; j++) {
+    //         tempStr += noKTP[j]
+    //       }
+    //       if (tempStr === namaInput) {
+    //         result.push(person)
+    //         found = true
+    //         break;
+    //       }
+    //     }
+    //   }
+    // }
+  }
+  if (result.length < 1) {
+    return 'Data tidak ditemukan'
+  }
+  return result
+}
